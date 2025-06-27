af3ea3b940bbdf67fc5e40d06a6034682eaf221a
Noman <nmazhar911@gmail.com>
2025-06-27 07:55:18 +0500

    using mlflow with a remote server of dagshub

--- src/mlflow_with_dagshub.py
+++ src/mlflow_with_dagshub.py
@@ -1,4 +1,5 @@
 import mlflow
+import dagshub
 import mlflow.sklearn
 import matplotlib.pyplot as plt
 import mlflow.sklearn
@@ -10,7 +11,9 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 data = load_wine()
 # print(data)
-mlflow.set_tracking_uri('http://127.0.0.1:5000/')
+dagshub.init(repo_owner='nomanmazharr', repo_name='mlflow', mlflow=True)
+
+mlflow.set_tracking_uri('https://dagshub.com/nomanmazharr/mlflow.mlflow')
 X = data.data
 y = data.target
 
@@ -18,10 +21,10 @@ x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # print(x_train.shape)
 # print(x_test.shape)
-n_estimators=100
-max_depth=100
+n_estimators=20
+max_depth=50
 
-mlflow.set_experiment('Wine-Experiment')
+mlflow.set_experiment('Wine-Experiment-2')
 with mlflow.start_run():
     rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
     rf.fit(x_train, y_train)
@@ -46,7 +49,7 @@ with mlflow.start_run():
     mlflow.log_artifact('confusion_matrix.png')
     mlflow.log_artifact(__file__)
 
-    mlflow.sklearn.log_model(rf, "Random Forest Classifier")
+    mlflow.sklearn.save_model(rf, "Random Forest Classifier-2") # name should be different each time even if you're in separate experiments
     mlflow.set_tags({"author": "Noman", "Project":"Wine Classification"})
     print(accuracy)
 
